a36748eb4d5ef1786c633e6f315c1b9c0349b60a
IbrahimTufail64 <ibrahimsomeonenotimportant@gmail.com>
2024-07-23 23:08:50 +0500

    bytecode developed for the language

--- lib/chunk.h
+++ lib/chunk.h
@@ -2,19 +2,25 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
 typedef struct {
     int count;
     int capacity;
+    ValueArray constants;
+    int* lines;
     uint8_t * code;
 } Chunk;
 
+
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk,uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 void freeChunk(Chunk *chunk);
+int addConstant(Chunk* chunk, Value value);
 
 #endif 
--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include "lib/common.h"
 #include "lib/chunk.h" 
 #include "lib/debug.h"
+#include "lib/vm.h"
 
 int main(int argc, const char* argv[]){
-    Chunk chunk;
-    initChunk(&chunk);
-    writeChunk(&chunk,OP_RETURN);
+  
+  initVM();
+  
+  Chunk chunk;
+  initChunk(&chunk);
+  int constant = addConstant(&chunk,1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk,"test chunk");
-    freeChunk(&chunk);
+  disassembleChunk(&chunk,"test chunk");
+  interpret(&chunk);
+  freeVM();
+  freeChunk(&chunk);
     
     return 0;
 }
